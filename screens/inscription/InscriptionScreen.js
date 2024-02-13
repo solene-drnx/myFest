0d0371322bba476b7b7cacd6984e9dd55eac8802
Solène <138325816+solene-drnx@users.noreply.github.com>
2024-02-13 15:00:41 +0100

    Connexion form
    Connexion fonctionne avec la photo de profil

--- screens/inscription/InscriptionScreen.js
+++ screens/inscription/InscriptionScreen.js
@@ -1,150 +1,153 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image, Alert } from "react-native";
 import { auth } from "../../firebase";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import * as ImagePicker from "expo-image-picker";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import * as FileSystem from "expo-file-system";
 
-export default class InscriptionScreen extends React.Component {
-    state = {
-        user: {
-            name: "",
-            email: "",
-            password: "",
-            avatar: null,
-        },
-        errorMessage: null,
-    };
 
-    componentDidMount() {
-        this.getCameraPermission();
-    }
+function InscriptionScreen({ setNavSelectionne }) {
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [image, setImage] = useState(null);
+    const [uploading, setUploading] = useState(false);
 
-    getCameraPermission = async () => {
-        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== 'granted') {
-            alert('Sorry, we need camera roll permissions to make this work!');
-        }
-    }
-
-    handlePickAvatar = async () => {
-        const result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    const pickImage = async () => {
+        // no permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
         });
 
-        if (!result.cancelled) {
-            this.setState({ user: { ...this.state.user, avatar: result.uri } });
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
         }
-    }
+    };
 
-    handleInscription = () => {
-        const { name, email, password } = this.state.user;
-
+    const handleInscription = () => {
+        const { name, email, password } = user;
+    
+        // Vérifiez si une image a été sélectionnée.
+        if (image != null) {
+            uploadImageAsync(image)
+                .then(downloadURL => {
+                    // L'image a été uploadée et vous avez l'URL, vous pouvez maintenant créer l'utilisateur.
+                    createUser(email, password, name, downloadURL);
+                })
+                .catch(error => {
+                    console.error(error);
+                    setErrorMessage("Erreur lors de l'upload de l'image.");
+                });
+        } else {
+            // Aucune image n'a été sélectionnée, continuez avec la création de l'utilisateur sans image.
+            createUser(email, password, name);
+        }
+    };
+    
+    const uploadImageAsync = async (uri) => {
+        setUploading(true);
+        const filename = uri.substring(uri.lastIndexOf('/') + 1);
+        const blob = await (await fetch(uri)).blob();
+        const storage = getStorage();
+        const imageRef = storageRef(storage, `images/${filename}`);
+    
+        await uploadBytes(imageRef, blob);
+        const downloadURL = await getDownloadURL(imageRef);
+        setUploading(false);
+        return downloadURL;
+    };
+    
+    const createUser = (email, password, name, photoURL = null) => {
         createUserWithEmailAndPassword(auth, email, password)
             .then(userCredentials => {
-                this.uploadAvatar(userCredentials.user);
+                const user = userCredentials.user;
+                updateProfile(user, {
+                    displayName: name,
+                    ...(photoURL && { photoURL }) // Ajoute l'URL de la photo de profil si disponible.
+                }).then(() => {
+                    console.log('Profile updated!');
+                    setNavSelectionne("profil");
+                }).catch(error => {
+                    console.error(error);
+                    setErrorMessage(error.message);
+                });
             })
             .catch(error => {
-                this.setState({ errorMessage: error.message });
+                console.error(error);
+                setErrorMessage(error.message);
             });
     };
 
-    uploadAvatar = async (user) => {
-        const { avatar, name } = this.state.user;
-        if (avatar) {
-            const response = await fetch(avatar);
-            const blob = await response.blob();
-            const storage = getStorage();
-            const storageRef = ref(storage, `avatars/${user.uid}`);
-            uploadBytes(storageRef, blob).then(snapshot => {
-                getDownloadURL(snapshot.ref).then(downloadURL => {
-                    updateProfile(user, {
-                        displayName: name,
-                        photoURL: downloadURL
-                    }).then(() => {
-                        console.log('Profile updated!');
-                        this.props.setNavSelectionne("profil");
-                    });
-                });
-            }).catch(error => {
-                console.error("Error uploading avatar: ", error);
-                this.setState({ errorMessage: error.message });
-            });
-        } else {
-            updateProfile(user, {
-                displayName: name,
-            }).then(() => {
-                console.log('Profile updated without avatar!');
-                this.props.setNavSelectionne("profil");
-            });
-        }
-    };
-    render() {
-        return (
-            <View style={style.container}>
-                <Text style={style.greeting}>{"Hello 👋\n Inscris toi pour commencer"}</Text>
-                <TouchableOpacity style={style.avatarPlaceholder} onPress={this.handlePickAvatar}>
-                    <Image source={{ uri: this.state.user.avatar }} style={style.avatar} />
-                    <Ionicons
-                        name="add"
-                        size={40}
-                        color="#FFF">
+    return (
+        <View style={style.container}>
+            <Text style={style.greeting}>{"Hello 👋\n Inscris toi pour commencer"}</Text>
 
-                    </Ionicons>
-                </TouchableOpacity>
-
-                <View style={style.errorMessage}>
-                    {this.state.errorMessage && <Text style={style.error}>{this.state.errorMessage}</Text>}
-                </View>
-
-                <View style={style.form}>
-                    <View>
-                        <Text style={style.inputTitle}>Prénom</Text>
-                        <TextInput
-                            style={style.input}
-                            autoCapitalize="none"
-                            onChangeText={name => this.setState({ name })}
-                            value={this.state.name}
-                        />
-
-                    </View>
-                    <View style={{ marginTop: 32 }}>
-                        <Text style={style.inputTitle}>Adresse mail</Text>
-                        <TextInput
-                            style={style.input}
-                            autoCapitalize="none"
-                            onChangeText={email => this.setState({ email })}
-                            value={this.state.email}>
-                        </TextInput>
-                    </View>
-                    <View style={{ marginTop: 32 }}>
-                        <Text style={style.inputTitle}>Mot de passe</Text>
-                        <TextInput
-                            style={style.input}
-                            secureTextEntry
-                            autoCapitalize="none"
-                            onChangeText={password => this.setState({ password })}
-                            value={this.state.password}
-                        >
-                        </TextInput>
-                    </View>
-                </View>
-
-                <TouchableOpacity style={style.button} onPress={this.handleInscription}>
-                    <Text style={{ color: "#FDF4EB", fontFamily: "Montserrat-Medium" }}>S'inscrire</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={{ alignSelf: "center", marginTop: 32 }} onPress={() => this.props.setNavSelectionne("connexion")}>
-                    <Text style={{ color: "#414959", fontSize: 13, fontFamily: "Montserrat-Medium" }}>Tu as déjà un compte ?<Text style={{ color: "#F57C33" }}> Connexion</Text></Text>
-                </TouchableOpacity>
+            <View style={style.errorMessage}>
+                {errorMessage && <Text style={style.error}>{errorMessage}</Text>}
             </View>
 
-        )
-    }
+            <View style={style.container_imagePicker}>
+            <TouchableOpacity style={style.selectButton} onPress={pickImage}>
+                <Ionicons
+                    name="add"
+                    size={40}
+                    color="#FFF">
+
+                </Ionicons>
+            </TouchableOpacity>
+            <View style={style.imageContainer}>
+                {image && <Image source={{ uri: image }} style={{ width: 100, height: 100, borderRadius: 50, marginTop: -100 }} />}
+            </View>
+        </View>
+
+            <View style={style.form}>
+                <View>
+                    <Text style={style.inputTitle}>Prénom</Text>
+                    <TextInput
+                        style={style.input}
+                        autoCapitalize="none"
+                        onChangeText={name => setUser({ ...user, name })}
+                        value={user.name}
+                    />
+                </View>
+                <View style={{ marginTop: 32 }}>
+                    <Text style={style.inputTitle}>Adresse mail</Text>
+                    <TextInput
+                        style={style.input}
+                        autoCapitalize="none"
+                        onChangeText={email => setUser({ ...user, email })}
+                        value={user.email}
+                    />
+                </View>
+                <View style={{ marginTop: 32 }}>
+                    <Text style={style.inputTitle}>Mot de passe</Text>
+                    <TextInput
+                        style={style.input}
+                        secureTextEntry
+                        autoCapitalize="none"
+                        onChangeText={password => setUser({ ...user, password })}
+                        value={user.password}
+                    />
+                </View>
+            </View>
+
+            <TouchableOpacity style={style.button} onPress={handleInscription}>
+                <Text style={{ color: "#FDF4EB", fontFamily: "Montserrat-Medium" }}>S'inscrire</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{ alignSelf: "center", marginTop: 32 }} onPress={() => setNavSelectionne("connexion")}>
+                <Text style={{ color: "#414959", fontSize: 13, fontFamily: "Montserrat-Medium" }}>Tu as déjà un compte ?<Text style={{ color: "#F57C33" }}> Connexion</Text></Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 const style = StyleSheet.create({
@@ -194,19 +197,28 @@ const style = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
     },
-    avatarPlaceholder: {
+    container_imagePicker: {
+        marginTop: -40,
+        marginBottom: 20,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    selectButton: {
         width: 100,
         height: 100,
         backgroundColor: "#E1E2E6",
         borderRadius: 50,
-        marginTop: 48,
         justifyContent: "center",
         alignItems: "center",
     },
-    avatar: {
-        position: "absolute",
-        width: 100,
-        height: 100,
-        borderRadius: 50,
+    buttonText: {
+        color: "#fff",
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    imageContainer: {
+        alignItems: "center",
     }
-})
+});
+
+export default InscriptionScreen;
--- screens/profil/ProfilScreen.jsx
+++ screens/profil/ProfilScreen.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Image } from "react-native";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import ConnexionScreen from "../connexion/ConnexionScreen";
-import UploadImage from "../../components/UploadImage/UploadImage";
 
 const auth = getAuth(); // Assurez-vous que ceci est bien importé de votre configuration Firebase
 
 export function ProfilScreen({ setNavSelectionne }) {
-    /* const [user, setUser] = useState(null);
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             if (currentUser) {
-                // Utilisateur connecté
+                // Utilisateur connecté, mettez à jour l'état de l'utilisateur avec les nouvelles données
                 setUser({
                     email: currentUser.email,
                     displayName: currentUser.displayName,
+                    photoURL: currentUser.photoURL, // Ajoutez l'URL de la photo de profil
                 });
             } else {
                 // Utilisateur non connecté
@@ -32,20 +32,23 @@ export function ProfilScreen({ setNavSelectionne }) {
     };
 
     if (!user) {
-        // Si aucun utilisateur n'est connecté, on pourrait rediriger vers le ConnexionScreen
-        // ou simplement afficher un bouton de connexion.
+        // Si aucun utilisateur n'est connecté, afficher le ConnexionScreen ou un bouton de connexion
         return <ConnexionScreen setNavSelectionne={setNavSelectionne} />;
     }
 
-    // Affiche les informations de l'utilisateur et un bouton de déconnexion si un utilisateur est connecté
+    // Affiche les informations de l'utilisateur, y compris la photo de profil, et un bouton de déconnexion
     return (
-        <View>
-            <Text>Hello, {user.email || "User"}!</Text>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            {user.photoURL ? (
+                <Image
+                    source={{ uri: user.photoURL }}
+                    style={{ width: 100, height: 100, borderRadius: 50 }} // Style pour rendre l'image ronde
+                />
+            ) : (
+                <Text>No profile picture</Text> // Fallback si aucune photo de profil n'est disponible
+            )}
+            <Text>Hello, {user.displayName || user.email || "User"}!</Text>
             <Button title="Sign Out" onPress={signOutUser} />
         </View>
-    );
-    */
-    return(
-        <UploadImage/>
     );
 }
