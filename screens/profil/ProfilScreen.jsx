f1ac699db1d5fc76866d465207ae8e24c973ebe5
Solène <138325816+solene-drnx@users.noreply.github.com>
2024-02-13 21:45:24 +0100

    Profil UI
    modifications sur le design de l'écran profil

--- screens/profil/ProfilScreen.jsx
+++ screens/profil/ProfilScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Button, Image } from "react-native";
+import { View, Text, Button, Image, TouchableOpacity } from "react-native";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import ConnexionScreen from "../connexion/ConnexionScreen";
 
@@ -37,14 +37,18 @@ export function ProfilScreen({ setNavSelectionne }) {
             {user.photoURL ? (
                 <Image
                     source={{ uri: user.photoURL }}
-                    style={{ width: 150, height: 150, borderRadius: 100, marginBottom: 40}} 
+                    style={{ width: 150, height: 150, borderRadius: 100, marginBottom: 20}} 
                 />
             ) : (
                 <Text>No profile picture</Text> 
             )}
-            <Text style={{fontFamily: "Montserrat-Black", fontSize: 24}}>Hello {user.displayName || user.email || "User"} 👋</Text>
-            <Text style={{fontFamily: "Montserrat-Medium", marginTop: 20,}}>Bienvenue sur MyFest, l'application pour vos festivals !</Text>
-            <Button title="Sign Out" onPress={signOutUser} />
+            <Text style={{fontFamily: "Montserrat-Black", fontSize: 24, color: "#F57C33", marginHorizontal: 40}}>Hello {user.displayName || user.email || "User"} 👋</Text>
+            <Text style={{fontFamily: "Montserrat-Medium", marginTop: 40, marginHorizontal: 40}}>Bienvenue sur MyFest, l'application pour vos festivals !</Text>
+            <Text style={{fontFamily: "Montserrat-Medium", marginTop: 20, marginHorizontal: 40}}>Pour démarrer le jeu, direction l'onglet 'Cards' situé en bas à gauche, et amusez-vous à swiper quelques cartes.</Text>
+            <Text style={{fontFamily: "Montserrat-Medium", marginTop: 20, marginHorizontal: 40}}>Ensuite, faites un petit tour dans l'onglet du milieu pour découvrir le programme qu'on vous a concocté sur mesure.</Text>
+            <TouchableOpacity onPress={signOutUser} style={{backgroundColor: "#F57C33", paddingVertical: 15, paddingHorizontal: 25, borderRadius: 5, marginTop: 40}}>
+                <Text style={{fontFamily: "Montserrat-Medium", color: "#FDF4EB"}}>déconnection</Text>
+            </TouchableOpacity>
         </View>
     );
 }
